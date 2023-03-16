b202d92cffcea2c9a7410c2b87cf2f9314ac3686
Unero <ramadhanbhg@gmail.com>
2023-03-16 16:13:50 +0700

    fix ch1s4 output

--- ch1/challenge/biodata.go
+++ ch1/challenge/biodata.go
@@ -11,21 +11,21 @@ type Biodata struct {
 }
 
 func showBiodata(biodata Biodata) {
-	fmt.Println("Name: ", biodata.Name)
-	fmt.Println("Address: ", biodata.Address)
-	fmt.Println("Job: ", biodata.Job)
-	fmt.Println("Motive: ", biodata.Motive)
+	fmt.Println("Name\t: ", biodata.Name)
+	fmt.Println("Address\t: ", biodata.Address)
+	fmt.Println("Job\t: ", biodata.Job)
+	fmt.Println("Motive\t: ", biodata.Motive)
 }
 
 func main() {
 	var args = os.Args[1:][0]
 
-	dict := map[string]Biodata{
+	data := map[string]Biodata{
 		"1": {"Rizky", "Jakarta", "Programmer", "Learn Go"},
-		"2": {"Rizky", "Jakarta", "Programmer", "Learn Go"},
-		"3": {"Rizky", "Jakarta", "Programmer", "Learn Go"},
-		"4": {"Rizky", "Jakarta", "Programmer", "Learn Go"},
+		"2": {"Nero", "Malang", "Programmer", "Learn Go"},
+		"3": {"Dimas", "Bandung", "Programmer", "Learn Go"},
+		"4": {"Fadya", "Solo", "Programmer", "Learn Go"},
 	}
 
-	showBiodata(dict[args])
+	showBiodata(data[args])
 }
