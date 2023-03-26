41bc282d8026d8665b05a256812a05fc3c650d2e
Unero <ramadhanbhg@gmail.com>
2023-03-26 23:07:43 +0700

    complete swagger

---
+++ web-swagger/models/car.go
@@ -0,0 +1,10 @@
+package models
+
+// Car represents the model for an car
+type Car struct {
+	Pemilik  string `gorm:"varchar(100)"`
+	Merk     string `gorm:"varchar(100)"`
+	Harga    int    `gorm:"integer(11)"`
+	Typecars string `gorm:"varchar(100)"`
+	Id       uint   `gorm:"primary_key"`
+}
