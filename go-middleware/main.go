c56012be492812285a93c33be41389b55e098e9a
Unero <ramadhanbhg@gmail.com>
2023-03-27 19:22:24 +0700

    middleware

---
+++ go-middleware/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	endpoint := http.HandlerFunc(greet)
+
+	mux.Handle("/", middleware1(middleware2(endpoint)))
+	fmt.Println("Server started at localhost:8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
+func middleware1(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("middleware pertama")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middleware2(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("middleware kedua")
+		next.ServeHTTP(w, r)
+	})
+}
